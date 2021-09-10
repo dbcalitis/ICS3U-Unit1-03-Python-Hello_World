25ba5adc8a97490870adaee7166006580b93adcb
Ubuntu <ubuntu@ip-172-31-81-123.ec2.internal>
2021-09-10 16:22:37 +0000

    added hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by : Daria Bernice Calitis
+# Created on: Sept. 2021
+# This is the Hello World program
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
